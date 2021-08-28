ec3943b1c44820e2be4999965364804f589fffd6
Juan <juand8012@gmail.com>
2021-08-28 04:12:31 -0500

    Products were mapped

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,3 +1,4 @@
+import { ProductI } from './interfaces/product.interface';
 import { ProductsService } from './services/products.service';
 import { Component, OnInit } from '@angular/core';
 import { tap } from 'rxjs/operators';
@@ -8,12 +9,13 @@ import { tap } from 'rxjs/operators';
   styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit {
+  products: ProductI[] | undefined;
   constructor(private productSvc: ProductsService) {}
 
   ngOnInit(): void {
     this.productSvc
       .getProducts()
-      .pipe(tap((res) => console.log(res)))
+      .pipe(tap((product: ProductI[]) => (this.products = product)))
       .subscribe();
   }
 }
